50ff56c07925e0fc00457b27d89c47cfdc447bb1
AliphaticGuru <sirajmuhd2@gmail.com>
2025-10-07 10:27:57 +0100

    vite activated, docker image sanitized

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,11 +3,43 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', 
+        // origin: 'http://host.docker.internal:5173',
+        strictPort: true,
+        port: 5173,
+        hmr: {
+            host: process.env.VITE_DEV_SERVER_HOST || 'localhost',
+            protocol: 'ws',
+            port: 5173,
+        },
+        // watch: {
+        //     usePolling: true,
+        // }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        tailwindcss()
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
+    },
 });
+
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+// });
