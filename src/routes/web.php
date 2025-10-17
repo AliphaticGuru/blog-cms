79c28bd22ff1513fdabbeb3c59f94965207e2027
AliphaticGuru <sirajmuhd2@gmail.com>
2025-10-17 19:29:26 +0100

    blog landing implemented, ci now uses env values)

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::view('home', 'home')->name('home');
+
+Route::view('about', 'about')->name('about');
+
+Route::view('contact', 'contact')->name('contact');
+
+Route::view('article', 'article')->name('article');
